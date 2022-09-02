861136f3ee7c5750e7b67a1def3cf0b565b74530
widdu <vlehdonmaki@gmail.com>
2022-09-03 01:44:22 +0300

    Input processor scripti pois käytöstä, tilalle Player Input setting.
    PlayerInput scriptin rigidbody add force metodit FixedUpdate() metodissa saa arvonsa Update() metodista.
    
    Pelaajalle lisätty materiaaliksi PlayerMaterial, jossa friction 0.4 ja bounciness 0.
    
    Input system konfiguraatio.
    Keyboard ja gamepad schemet.

--- Assets/Code/PlayerInput.cs
+++ Assets/Code/PlayerInput.cs
@@ -6,12 +6,13 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     // Private variables
-    private float moveSpeed = 10f;
-    private Vector2 moveInput, jumpInput, movement;
+    private Vector2 moveInput, jumpInput, dropInput, movement, movementXvector;
     private new Rigidbody2D rigidbody2D;
+    private float movementX, movementY;
 
     // Public variables
-    public bool grounded = true;
+    public float moveSpeed = 10f, jumpForce = 5f;
+    public bool grounded = false;
 
     void Awake()
     {
@@ -22,60 +23,76 @@ public class PlayerInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Movement values
+        movementX = moveInput.x * moveSpeed * Time.fixedDeltaTime;
+        movementY = jumpInput.y * jumpForce;
+        movement = new Vector2(movementX, movementY);
+        movementXvector = new Vector2(movementX, 0f);
     }
 
     void FixedUpdate()
     {
-        PlayerMove();
-        PlayerJump();
+        PlayerMove(movement, movementXvector);
     }
 
-    void PlayerMove()
+    void PlayerMove(Vector2 movement, Vector2 movementXvector)
     {
         // Check if grounded
         if(grounded)
         {
-            movement = moveInput * moveSpeed * Time.fixedDeltaTime;
+                rigidbody2D.AddForce(movement, ForceMode2D.Impulse);
         }
-
-        // Add movement to position
-        rigidbody2D.MovePosition(rigidbody2D.position + movement);
+        rigidbody2D.AddForce(movementXvector, ForceMode2D.Impulse);
     }
 
-    void PlayerJump()
+    bool IsGrounded()
     {
-        // Readability
-        Vector2 upJumpInput = jumpInput.y * transform.up * moveSpeed * Time.fixedDeltaTime;
-
-        // Jump
-        if (upJumpInput.y > 0f && grounded) 
-        {
-            rigidbody2D.AddForce(upJumpInput, ForceMode2D.Impulse);
-            Debug.Log("upJump: " + upJumpInput);
-            Debug.Log("Adding to force");
-        }
-
-        // Set grounded
-        grounded = Mathf.Approximately(Mathf.Abs(rigidbody2D.velocity.y), 0f);
-        Debug.Log("yVelocity: " + Mathf.Abs(rigidbody2D.velocity.y));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, 2);
+        return hit.collider != null && Mathf.Approximately(rigidbody2D.velocity.y, 0f);
     }
 
-    private void OnMove(InputAction.CallbackContext callBackContext)
+    // Return input action values
+    public void Move(InputAction.CallbackContext callBackContext)
     {
         moveInput = callBackContext.ReadValue<Vector2>();
         //Debug.Log("Move input " + moveInput);
     }
-
-    private void OnJump(InputAction.CallbackContext callBackContext)
+    public void Jump(InputAction.CallbackContext callBackContext)
     {
         jumpInput = callBackContext.ReadValue<Vector2>();
-        //Debug.Log("Jump input " + jumpInput);
+        //Debug.Log("Move input " + moveInput);
+    }
+    public void Drop(InputAction.CallbackContext callBackContext)
+    {
+        dropInput = callBackContext.ReadValue<Vector2>();
+        //Debug.Log("Move input " + moveInput);
+    }
+
+    // Return collision target values
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Enter: " + collision);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // Check if grounded and set bool
+        grounded = IsGrounded();
+
+        //Debug.Log("Stay: " + collision);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // Set grounded to false up exiting collision
+        grounded = false;
+
+        //Debug.Log("Exit: " + collision);
     }
 }
